9d2f845dff8d3b4d2c3879376adf4c8f62a679f9
KevinRojas1499 <kevinrojas99@gmail.com>
2023-04-10 19:15:35 +0000

    Cleaning up

--- scripts/infer/cascaded_infer.sh
+++ scripts/infer/cascaded_infer.sh
@@ -1,3 +1,3 @@
 torchrun --standalone --nnodes=1 --nproc_per_node=8 generate.py --outdir=ffhq_cascaded_generation --seeds=0-63 --batch=64 \
-    --network https://drive.google.com/file/d/1B4r0ibrgh3EoqyaLVMQxSfCmOibu_9k9/view?usp=sharing \
+    --network ffhq-training-runs/network-snapshot-073382.pkl \
     --img_resolution 32
