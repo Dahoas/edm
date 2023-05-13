f07e3ce130b9b551372e907803a5f22d1a2bd64a
KevinRojas1499 <kevinrojas99@gmail.com>
2023-05-13 15:42:43 +0000

    Lsun training files

---
+++ scripts/train/multi_res/run_small_dual_church.sh
@@ -0,0 +1,5 @@
+torchrun --standalone --nproc_per_node=8 train.py --outdir=lsun_church_training-runs \
+    -d datasets/lsun_church_96x96/ -d datasets/lsun_church_80x80/ -d datasets/lsun_church_64x64/ -d datasets/lsun_church_48x48/ -d datasets/lsun_church_32x32/ \
+     --arch=ddpmpp --mode="dual" --batch 256 \
+    --model_config_path model_configs/small_church.yml \
+    --lr 1e-3 --tick 25 --snap 25
